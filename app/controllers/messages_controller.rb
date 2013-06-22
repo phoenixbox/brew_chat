fc1d2fa82e72bd6ac7b454fbc119e2a41fbb140c
Josh Mejia <joshua.mejia@gmail.com>
2013-06-22 12:14:43 -0600

    merge changes from chris' branch

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   def index
-    @messages = Message.all
+    @messages = Message.where(:game_id => params[:game_id])
     render :json => @messages
   end
 end
